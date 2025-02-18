200f9eb52b96603932e7afb6a75255a3e633c384
Corey Lofthus <corey@soulyftaudio.com>
2025-02-17 21:24:13 -0800

    Initial Commit

---
+++ VolumeApp/VolumeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  VolumeAppApp.swift
+//  VolumeApp
+//
+//  Created by Corey Lofthus on 2/17/25.
+//
+
+import SwiftUI
+
+@main
+struct VolumeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
